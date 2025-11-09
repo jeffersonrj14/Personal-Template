669602827c0e849f1dbf79b476b4c6074d2cd118
jeffersonrj14 <stupidjeff430@gmail.com>
2025-11-09 16:59:39 +0700

    fix(lint): remove typescript-eslint parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,24 @@
-import next from '@next/eslint-plugin-next'
-import prettier from 'eslint-plugin-prettier'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
+import next from "@next/eslint-plugin-next"
+import prettier from "eslint-plugin-prettier"
 
 export default [
   {
-    ignores: ['node_modules', '.next', 'out', 'dist']
+    ignores: ["node_modules", ".next", "out", "dist"]
   },
   {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json'
-      }
-    },
     plugins: {
-      '@typescript-eslint': tseslint,
-      '@next/next': next,
+      "@next/next": next,
       prettier
     },
     rules: {
-      ...next.configs['core-web-vitals'].rules,
-      ...tseslint.configs.recommended.rules,
-      'prettier/prettier': ['error', { endOfLine: 'auto' }]
+      ...next.configs["core-web-vitals"].rules,
+      "prettier/prettier": ["error", { endOfLine: "auto" }]
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
     }
   }
 ]
